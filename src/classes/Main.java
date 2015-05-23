024ff76463b173c71c56e210bd494dfe80ccd764
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2015-05-23 16:52:42 +0400

    Nick Commit in new brach

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -6,5 +6,6 @@ package classes;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello NIka");
+        System.out.println("Kide erti cvlileba");
     }
 }
